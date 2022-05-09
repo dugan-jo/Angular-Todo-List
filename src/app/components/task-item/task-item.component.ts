b3c3714d5c3a5128334e903849dd63249172f848
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-05-09 12:16:14 -0600

    CRUD working

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
 import { Task } from '../../Task'
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-task-item',
@@ -9,13 +10,20 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 })
 export class TaskItemComponent implements OnInit {
   //declaring the task item in the component. 
-  @Input() task!: Task
+  @Input() task?: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output()  onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onDelete(task) {
+    this.onDeleteTask.emit(task)
+  }
+  onToggle(task) {
+this.onToggleReminder.emit(task);
+  }
 
 }
